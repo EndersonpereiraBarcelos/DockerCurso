4e7cd233077478323401b6da6ef866d0b0058d2a
pbenderson <souza98120@gmail.com>
2021-11-27 11:44:01 -0300

    .

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,8 @@ COPY package.json .
 
 RUN npm install 
 
-COPY . .
+COPY . . 
 
 EXPOSE  3000
 
-CMD ["node","index.js"]
+CMD ["node","index.js","index.html"]
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const router = express.Router();
 
-app.get('/',(req,res)=>{
-    res.send('Olá docker!, atualização')
-})
+app.get('/', (req, res) => {
+  res.send('Olá Docker! Atualização!')
+});
 
-app.listen(port,()=>{
-    console.log(`app rodando na porta:${port}`)
+
+
+app.listen(port, () => {
+  console.log(`App rodando na porta: ${port}`)
 })
